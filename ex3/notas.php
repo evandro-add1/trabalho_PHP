947c6ad858f584ca727daee702bdfe2f02ac9565
Evandro <evandroadd!@gmail.com>
2026-03-23 20:06:32 -0300

    atualização super

--- ex3/notas.php
+++ ex3/notas.php
@@ -1,12 +1,26 @@
 <?php
+
+if (empty ($_POST[nota] || $_POST[nota2] || $_POST[nota])){
+    echo "Digite um numero para calcular a média.";
+    echo "<br> <a href=\"index.php\">Novo caculo média.</a>";
+};
+
 $nota = (int) $_POST["nota"];
 $nota2 = (int) $_POST["nota2"];
 $nota3 = (int) $_POST["nota3"];
 
-$total = $nota + $nota2 + $nota3;
+$media = ($nota + $nota2 + $nota3)/ 3;
 
-$media = $total / 3;
 
-echo "Notas: ". $nota. ",". $nota2 .",". $nota3."\nMedia das suas notas: ". $media;
+if ($media > 7){
+    echo "Parabéns, não fez mais que sua obrigação.";
+    echo "Notas: ". $nota. ",". $nota2 .",". $nota3."\nMedia das suas notas: ". $media;
+} else {
+    echo "Reprovado!.";
+    echo "Notas: ". $nota. ",". $nota2 .",". $nota3."\nMedia das suas notas: ". $media;
+}
+
 
 ?>
+<br>
+<a href="index.php"></a>
